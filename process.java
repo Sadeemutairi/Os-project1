a3b689803e879260a33738bc1f777ee8651817eb
fayyatheeb <fay123412@hotmail.com>
2025-03-05 19:21:20 +0300

    Update process.java

--- process.java
+++ process.java
@@ -1,15 +1,14 @@
 import java.util.*;
-import java.util.PriorityQueue;
 
-public class process implements Comparable<process> {
+class Process implements Comparable<Process> {
     int processId;
     int arrival_Time;
     int burst_Time;
     int remaining_Time;
     int start_Time = -1;
-    int finish_Time;
+    int finish_Time = -1;
 
-    public process(int processId, int arrivalTime, int burstTime) {
+    public Process(int processId, int arrivalTime, int burstTime) {
         this.processId = processId;
         this.arrival_Time = arrivalTime;
         this.burst_Time = burstTime;
@@ -24,10 +23,21 @@ public class process implements Comparable<process> {
         return getTurnaround() - burst_Time;
     }
 
-    public int compareTo(process other) {
+    @Override
+    public int compareTo(Process other) {
         if (this.remaining_Time != other.remaining_Time) {
             return this.remaining_Time - other.remaining_Time;
         }
         return this.arrival_Time - other.arrival_Time;
     }
+
+    @Override
+    public String toString() {
+        return "P" + processId;
+    }
 }
+
+
+
+
+
